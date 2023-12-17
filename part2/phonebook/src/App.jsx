3d1027996a118aa27d9517ae83f6883dea85f76c
Alina-02 <alinamontma@gmail.com>
2023-12-17 10:47:41 +0100

    css

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import Filter from "./components/Filter";
+import personsService from "./services/persons";
+import Notification from "../../notes/src/components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFind, setNewFind] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const [filterPersons, setFilterPersons] = useState(persons);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      const per = response.data;
-      setPersons(per);
-      setFilterPersons(per);
+    personsService.getAll().then((response) => {
+      setPersons(response.data);
+      setFilterPersons(response.data);
     });
   }, []);
 
@@ -27,6 +28,7 @@ const App = () => {
       number: newNumber,
     };
 
+    //checks if the person is already in the database
     const personsCopy = persons;
     const personsFilter = personsCopy.filter(
       (person) => person.name == nameObject.name
@@ -35,11 +37,37 @@ const App = () => {
     if (personsFilter.length > 0) {
       window.alert(`${nameObject.name} is already added to phonebook`);
     } else {
+      console.log("entra");
+      //save the numbers to a backend server
+      personsService.create(nameObject).then((response) => {
+        setPersons(persons.concat(response.data));
+        setFilterPersons(persons.concat(response.data));
+        setNewName("");
+        setNewNumber("");
+      });
       setPersons(persons.concat(nameObject));
       setFilterPersons(persons.concat(nameObject));
+      setErrorMessage(`Added ${nameObject.name}`);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
     }
+
     setNewName("");
     setNewNumber("");
+  };
+
+  const deletePerson = (id) => {
+    //delete the person from the backend and set the persons again
+    if (window.confirm("Delete Arto Hellas?")) {
+      personsService.deleted(id).then((response) => {
+        personsService.getAll().then((response) => {
+          setPersons(response.data);
+          console.log(response.data);
+          setFilterPersons(response.data);
+        });
+      });
+    }
   };
 
   const handlePersonChange = (event) => {
@@ -60,6 +88,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter newFind={newFind} handleFindChange={handleFindChange} />
 
       <h2>add a new</h2>
@@ -71,7 +100,7 @@ const App = () => {
         addName={addName}
       />
       <h2>Numbers</h2>
-      <Persons filterPersons={filterPersons} />
+      <Persons filterPersons={filterPersons} deletePerson={deletePerson} />
     </div>
   );
 };
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-const Persons = ({ filterPersons }) => {
+const Persons = ({ filterPersons, deletePerson }) => {
   return (
     <div>
       <ul>
         {filterPersons.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
+            <button
+              onClick={() => {
+                if (person.id != undefined) {
+                  deletePerson(person.id);
+                }
+              }}
+            >
+              delete
+            </button>
           </li>
         ))}
       </ul>
